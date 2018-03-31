2adec74270d4323ce2c308a2b1fc9756ae5d33fe
Chris Pauley <chrispauley92@gmail.com>
2018-03-31 15:15:37 -0500

    new way of deploying with cdn

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    devServer: {
-        host: '0.0.0.0',
-        disableHostCheck:true,
-        port:8081
-    }
+	devServer: {
+		host: '0.0.0.0',
+		disableHostCheck:true,
+		port:8081
+	},
+	configureWebpack: config => {
+		if (process.env.NODE_ENV === 'production') {
+			config.output.publicPath = 'https://securecdn.cc/';
+		} else {
+			config.output.publicPath = '/';
+		}
+	}
 }
